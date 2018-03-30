3a0279f2227ee5180f185f85525c0c3d702f1b6e
Ilya A. Volynets-Evenbakh <ilya@total-knowledge.com>
2018-03-30 12:03:43 -0700

    Use local sloth for running the annotation tool

--- input/annotate.sh
+++ input/annotate.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+MYDIR=$(cd $(dirname $0); pwd)
+export PYTHONPATH=$MYDIR/../sloth/sloth:$PYTHONPATH
+export PATH=$MYDIR/../sloth/sloth/bin:$PATH
 sloth --config whale_sloth.py whales.json
