510e1e76664c74e1f3daaacc8d1aa2897f72f77e
Ilya A. Volynets-Evenbakh <ilya@total-knowledge.com>
2018-04-20 06:40:42 -0700

    Save exported models into step-specific directory

--- input/export-model.sh
+++ input/export-model.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
+function die() {
+    echo $@
+    exit 1
+}
+export CUDA_VISIBLE_DEVICES=''
+# get the latest checkpoint
+checkpoint=$(ls google_object_detection/mask/mask_rcnn_inception_resnet_v2_atrous_coco/model.ckpt-*.index|sort -n|tail -n1)
+[ -n "${checkpoint}" ] || die "Could not find a checkpoint"
+checkpoint=${checkpoint/.index/}
+step=$(basename ${checkpoint})
+step=${step/model.ckpt-/}
+echo step=$step
 python3 ~/ai/tf-models/research/object_detection/export_inference_graph.py \
     --input_type image_tensor \
-    --pipeline_config_path google_object_detection/mask_rcnn_inception_resnet_v2_atrous_coco/pipeline.config \
-    --trained_checkpoint_prefix google_object_detection/mask_rcnn_inception_resnet_v2_atrous_coco/model.ckpt-10000 \
-    --output_directory mask_inference_graph.pb
+    --pipeline_config_path google_object_detection/mask/mask_rcnn_inception_resnet_v2_atrous_coco/pipeline.config \
+    --trained_checkpoint_prefix $checkpoint \
+    --output_directory mask_inference_graph/$step
+
+cp mask_inference_graph/$step/frozen_inference_graph.pb mask_inference_graph/
