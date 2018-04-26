d96e6698eb7297ba1f2400b7aadd4f929a88c3e8
Ilya A. Volynets-Evenbakh <ilya@total-knowledge.com>
2018-04-26 15:22:45 -0700

    Pass command line arguments to trainer

--- input/train_mask_model.sh
+++ input/train_mask_model.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec python3 ~/ai/tf-models/research/object_detection/train.py --logtostderr --pipeline_config_path=./mask_rcnn_inception_resnet_v2_atrous_coco_2018_01_28.config --train_dir=./google_object_detection/mask/mask_rcnn_inception_resnet_v2_atrous_coco 
+exec python3 ~/ai/tf-models/research/object_detection/train.py --logtostderr --pipeline_config_path=./mask_rcnn_inception_resnet_v2_atrous_coco_2018_01_28.config --train_dir=./google_object_detection/mask/mask_rcnn_inception_resnet_v2_atrous_coco $@
