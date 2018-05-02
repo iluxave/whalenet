e153b2738a6733ff3624b4e28e1906e0d6646cb7
Ilya A. Volynets-Evenbakh <ilya@total-knowledge.com>
2018-05-02 14:49:17 -0700

    Allow annotating more files

--- input/annotate.sh
+++ input/annotate.sh
@@ -3,4 +3,9 @@ MYDIR=$(cd $(dirname $0); pwd)
 export PYTHONPATH=$MYDIR/../sloth/sloth:$PYTHONPATH
 export PATH=$MYDIR/../sloth/sloth/bin:$PATH
 #python -m pdb $MYDIR/../sloth/sloth/bin/sloth --config whale_sloth.py whales.json
-sloth --config whale_sloth.py whales.json
+WHALES=whales.json
+if [ -f "$1" ]; then
+	WHALES="$1"
+fi
+
+sloth --config whale_sloth.py $WHALES
